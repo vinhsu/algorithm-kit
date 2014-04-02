378381a1089cd40154e144785b55124ef219b2d7
vhsu-hp <vhsu-hp@gmail.com>
2014-04-01 21:39:35 -0700

    simple quick sort

---
+++ src/com/vini/alg/sort/QuickSort.java
@@ -0,0 +1,77 @@
+package com.vini.alg.sort;
+
+public class QuickSort {
+
+	private QuickSort() {}
+	
+	public static void sort(Comparable[] a) {
+		Utility.shuffle(a);
+		sort(a, 0, a.length - 1);
+	}
+	
+	private static void sort(Comparable[] a, int lo, int hi) {
+		if (hi <= lo) return;
+		int j = partition(a, lo, hi);
+		sort(a, lo, j-1);
+		sort(a, j+1, hi);
+	}
+	
+	private static int partition(Comparable[] a, int lo, int hi) {
+		int i = lo;
+		int j = hi + 1;
+		Comparable v = a[lo];
+		while (true) {
+			while (less(a[++i], v))
+				if (i == hi) break;
+			
+			while (less(v, a[--j]))
+				if (j == lo) break;
+			
+			if (i >= j) break;
+			
+			exch(a, i, j);
+		}
+		
+		exch(a, lo, j);
+		return j;
+	}
+	
+    private static boolean less(Comparable v, Comparable w) {
+        return (v.compareTo(w) < 0);
+    }
+        
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+    
+	private static <T> void display(Comparable[] a) {
+        for (Comparable i : a) {
+            System.out.print(i+ ",");
+        }
+    }
+	
+	public static void main(String[] args) {
+		System.out.println("QuickSort:");
+		Integer[] a = {50, 34, 17, 99, 83, 246, 777, 13, 8, 57, 62};
+		
+		System.out.println("before :");
+		display(a);
+		System.out.println();
+		sort(a);
+		System.out.println("\nafter:");
+		display(a);
+		
+		System.out.println("\n\n----------------------------------");
+		String[] b = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+		System.out.println("before :");
+		display(b);
+		System.out.println();
+		sort(b);
+		System.out.println("\nafter:");
+		display(b);
+	}
+
+}
