0c518dcec07cc46e7e3f6d4eaa228414c640407e
vhsu-hp <vhsu-hp@gmail.com>
2014-04-07 17:51:45 -0700

    change Node to be generix type.

--- src/com/vini/alg/sort/OrderedLinkedListMaxPQ.java
+++ src/com/vini/alg/sort/OrderedLinkedListMaxPQ.java
@@ -1,7 +1,7 @@
 package com.vini.alg.sort;
 
-public class OrderedLinkedListMaxPQ {
-	private Node head;
+public class OrderedLinkedListMaxPQ<T extends Comparable<T>> {
+	private Node<T> head;
 	private int N;
 	OrderedLinkedListMaxPQ() {
 		head = null;
@@ -9,16 +9,16 @@ public class OrderedLinkedListMaxPQ {
 	
     public boolean isEmpty()   { return N == 0; }
     public int size()          { return N; }
-    public Node delMax() {
+    public Node<T> delMax() {
     	N--;
-    	Node m = head;
+    	Node<T> max = head;
     	head = head.next;
-    	return m;
+    	return max;
     }
     
-    public void insert(Node k)  { 
-    	Node prev = null;
-    	Node current = head;
+    public void insert(Node<T> k)  { 
+    	Node<T> prev = null;
+    	Node<T> current = head;
     	while(current != null && less(k, current))
         {
     		prev = current;
@@ -32,31 +32,41 @@ public class OrderedLinkedListMaxPQ {
         N++;
     }
 	
-    private boolean less(Node v, Node w) {
-        return (v.item.compareTo(w.item) < 0);
+    private boolean less(Node<T> v, Node<T> w) {
+        return (v.val.compareTo(w.val) < 0);
     }
     
 	public static void main(String[] args) {
-		OrderedLinkedListMaxPQ pq = new OrderedLinkedListMaxPQ();
+		OrderedLinkedListMaxPQ<Integer> pq = new OrderedLinkedListMaxPQ<Integer>();
 		Integer[] a = {50, 34, 17, 99, 83, 246, 777, 13, 8, 57, 62};
-		for (Integer i : a) {
-			Node node = pq.new Node(i);
-			pq.insert(node);
+		for (Integer val : a) {
+			pq.insert(new Node<Integer>(val));
 		}
 		
 		while (!pq.isEmpty()) 
-	        System.out.println(pq.delMax());
+	        System.out.print(pq.delMax() + ", ");
+		
+		System.out.println("\n---------------------------------");
+		 
+		OrderedLinkedListMaxPQ<String> pq2 = new OrderedLinkedListMaxPQ<String>();
+		String[] b = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+		for (String val : b) {
+			pq2.insert(new Node<String>(val));
+		}
+		
+		while (!pq2.isEmpty()) 
+	        System.out.print(pq2.delMax() + ", ");
 	}
 
-	class Node {
-		Node next;
-		Integer item;
-		Node(Integer item) {
-			this.item = item;
+	static class Node<T extends Comparable<T>> {
+		Node<T> next;
+		T val;
+		Node(T val) {
+			this.val = val;
 		}
 		
 		public String toString() {
-			return item.toString();
+			return val.toString();
 		}
 	}
 }
